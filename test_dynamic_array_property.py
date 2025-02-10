4b10a8cb7b4719116b321445b94af89fa5ce4cee
DESKTOP-M13T56A\Starsun Cao <c.x_yang@foxmail.com>
2025-02-10 20:21:04 +0300

    Optimized code style

--- test_dynamic_array_property.py
+++ test_dynamic_array_property.py
@@ -2,7 +2,8 @@ from hypothesis import given, strategies as st
 from dynamic_array import DynamicArray
 
 
-@given(st.lists(st.one_of(st.integers(), st.none())))  # Generate lists with ints and None
+# Generate lists with ints and None
+@given(st.lists(st.one_of(st.integers(), st.none())))
 def test_dynamic_array(lst):
     """Test from_list() and to_list() equivalence."""
     arr = DynamicArray(chunk_size=4, growth_factor=2)
